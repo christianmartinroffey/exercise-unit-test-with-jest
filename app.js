5a54854fb873fc0d05048000f7af766f74ecb7e3
christianmartinroffey <c.martinroffey@gmail.com>
2022-05-21 11:08:24 +0000

    added functions to app.js and test.js

--- app.js
+++ app.js
@@ -4,3 +4,45 @@ const sum = (a,b) => {
 };
 
 console.log(sum(4,3));
+
+// export the function to be used on other files 
+// (similar to the keyword `export` when using webpack)
+
+module.exports = { sum };
+
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+// we declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
+};
+
+module.exports = { fromEuroToDollar };
+//
+const fromDollarToYen = function(valueInDollars){
+
+    let valueInYen = (valueInDollars /1.2) * 127.9;
+
+    return valueInYen;
+
+};
+
+module.exports = { fromDollarToYen };
+//
+const fromYenToPound = function(valueInYen){
+
+    let valueInPound = (valueInYen / 127.9) * 0.8 ;
+
+    return valueInPound;
+
+};
+
+module.exports = { fromYenToPound };
